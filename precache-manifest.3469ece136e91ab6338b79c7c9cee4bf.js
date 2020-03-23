d0dedac6c3a1e0865683c5023e70115ab254eeec
Ruditya <rudityap20@gmail.com>
2020-03-23 20:58:42 +0700

    Updates

---
+++ precache-manifest.3469ece136e91ab6338b79c7c9cee4bf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eee18f99e84b9bf80e4ff13f8be5fc3c",
+    "url": "/WEB3/index.html"
+  },
+  {
+    "revision": "b64dceca55853f6b49c5",
+    "url": "/WEB3/static/css/2.4b6e3ef2.chunk.css"
+  },
+  {
+    "revision": "5840e3db6b81195a1175",
+    "url": "/WEB3/static/css/main.500bb0ab.chunk.css"
+  },
+  {
+    "revision": "b64dceca55853f6b49c5",
+    "url": "/WEB3/static/js/2.b8253ffb.chunk.js"
+  },
+  {
+    "revision": "176c1a1a28b19c611d9701905d8d60b7",
+    "url": "/WEB3/static/js/2.b8253ffb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5840e3db6b81195a1175",
+    "url": "/WEB3/static/js/main.4705b948.chunk.js"
+  },
+  {
+    "revision": "fc358291596057d87c91",
+    "url": "/WEB3/static/js/runtime-main.4b57216b.js"
+  },
+  {
+    "revision": "fdeea1ed30e14beda96521eac6dc8d73",
+    "url": "/WEB3/static/media/tea-cup.fdeea1ed.svg"
+  }
+]);
